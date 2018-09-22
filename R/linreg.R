26e889ef72017d3d63f9f5e96d599960816f0a14
Anubhav Dikshit <anubhav.dikshit@live.in>
2018-09-23 01:39:37 +0200

    Initial commit

---
+++ R/linreg.R
@@ -0,0 +1,118 @@
+#' Create a linreg object
+#' @param formula formula for linear model
+#' @param data the dataset provided
+#' @field formula, formla for the linear model
+#' @field data, the dataset provided
+#' @field m_X, the matrix of explanatory variables
+#' @field m_Y, the matrix of dependent variable
+#' @field X_t, transpose of m_X
+#' @field XtX, inverse of matrix multiplication of m_X and X_t
+#' @field betaestimates, estimates of the parameters
+#' @field yfit, estimated y values
+#' @field residual, residuals computed by subtracting yfit from actual y values
+#' @field nparameters, number of parameters
+#' @field residualvariance, variance of the residuals
+#' @field residualstd, standard deviation of residuals
+#' @field betavariance, variance of beta estimates
+#' @field bb, diagonal of betavariance matrix
+#' @field tvalues, comptued t values per parameter
+#' @field pvalues, pvalues computed according to tvalues and pt function
+#' @field standardizedresiduals, standardized residuals
+#' @field sqrtresiduals, the square root of standardizedresiduals
+#' @return empty
+#' @export
+
+# Create class
+linreg <- setRefClass("linreg",
+                      fields = list(formula="formula",
+                                    data="data.frame",
+                                    m_X = "matrix",
+                                    m_Y="matrix",
+                                    Xt="matrix",
+                                    XtX="matrix",
+                                    betaestimates = "matrix",
+                                    yfit="matrix",
+                                    residual="matrix",
+                                    nparameters = "integer",
+                                    dof="integer",
+                                    residualt = "matrix",
+                                    residualvariance="numeric",
+                                    residualstd = "numeric",
+                                    betavariance = "matrix",
+                                    bb = "numeric",
+                                    tvalues="matrix",
+                                    pvalues= "matrix",
+                                    standardizedresiduals ="matrix",
+                                    sqrtstresiduals = "matrix"
+                                    ),
+                      methods = list(
+                        initialize = function(formula, data){
+                          #Independent and dependent variables
+                          m_X <<- model.matrix(formula, data)
+                          m_Y <<- as.matrix(data[all.vars(formula)[1]])
+                          # Transpose matrix X, and multiply + solve
+                          Xt <<- t(m_X)
+                          XtX <<- solve(Xt %*% m_X)
+                          betaestimates <<- XtX %*% Xt %*% m_Y
+                          # Estimate y
+                          yfit <<- m_X %*% betaestimates
+                          # Estimate residuals
+                          residual <<- m_Y - yfit
+                          # Determine degrees of freedom
+                          nparameters <<- length(betaestimates)
+                          dof <<- length(m_Y) - nparameters
+                          # Variances
+                          residualt <<- t(residual)
+                          residualvariance <<- as.numeric((residualt %*% residual) / dof)
+                          residualstd <<- sqrt(residualvariance)
+                          betavariance <<- residualvariance * XtX
+                          bb <<- diag(betavariance)
+                          # t-values
+                          tvalues <<- betaestimates/sqrt(bb)
+                          # p-values
+                          pvalues <<- pt(tvalues, df=dof, lower.tail = FALSE)
+                          # Standardized residuals for summary
+                          standardizedresiduals <<- residual / sd(residual)
+                          sqrtstresiduals <<- sqrt(abs(standardizedresiduals))
+                        },
+                        print = function(){
+                          cat(paste("Coeffiencts for linreg(formula = ", format(formula), ", data = ", deparse(substitute(data)), ") :\n\n ", sep = ""))
+                          setNames(round(betaestimates[1:nrow(betaestimates)],3),rownames(betaestimates))
+                        },
+                        resid = function(){
+                          return(as.vector(residual))
+                        },
+                        pred = function(){
+                          return(yfit)
+                        },
+                        coef = function(){
+                          vec <- as.vector(betaestimates)
+                          names(vec) <- colnames(m_X)
+                          return(vec)
+                        },
+                        plot = function(){
+                          plot1 <- ggplot(data.frame(yfit, residual), aes(y=residual, x=yfit)) + geom_point(shape=21, size=3, colour="black", fill="white")
+                          plot1 <- plot1 + theme_linedraw() + theme(panel.grid.major = element_blank(),
+                                                                            panel.grid.minor = element_blank(),
+                                                                            plot.title = element_text(hjust = 0.5))
+                          plot1 <- plot1 + stat_summary(fun.y=median, colour="red", geom="line", aes(group = 1))
+                          plot1 <- plot1 + ggtitle("Residuals vs fitted") + xlab(paste("Fitted values \n lm(Petal.Length ~ Species)"))
+                          plot2 <- ggplot(data.frame(yfit, sqrtstresiduals), aes(y=sqrtstresiduals, x=yfit)) + geom_point(alpha = 0.6, shape=21, size=3, colour="black", fill="white")
+                          plot2 <- plot2 + theme_linedraw() + theme(panel.grid.major = element_blank(),
+                                                                                 panel.grid.minor = element_blank(),
+                                                                                 plot.title = element_text(hjust = 0.5))
+                          plot2 <- plot2 + stat_summary(fun.y=median, colour="red", geom="line", aes(group = 1))
+                          plot2 <- plot2 + ggtitle("Scale-Location") + xlab(paste("Fitted values \n lm(Petal.Length ~ Species)"))
+                          plot2 <- plot2 + scale_x_continuous(breaks = seq(0.0, 1.5, by= 0.5))
+
+                          plotlist <- list(plot1, plot2)
+                          return(plotlist)
+                        },
+                        summary = function(){
+                          cat(paste("linreg(formula = ", format(formula), ", data = ", deparse(substitute(data)), ") :\n\n ", sep = ""))
+                          cat(paste("degrees of freedom: ", dof, "\n\n", sep = ""))
+                          setNames(
+                            as.data.frame(cbind(betaestimates,as.matrix(sqrt(bb)),tvalues,pvalues)),
+                            c("Coefficients","Standard error","t values", "p values"))
+                        }
+                      ))
